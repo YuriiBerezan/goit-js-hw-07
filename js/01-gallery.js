eaf2889a3475bfe3ac054bcf13891daad9649f8c
Yurii Berezan <67425022+YuriiBerezan@users.noreply.github.com>
2022-05-12 16:58:15 +0300

    add gallery markup to console

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+console.log(createGallerymarkup(galleryItems))
+
+function createGallerymarkup(items) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+          <div class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    }).join(' ');        
+}
+
+
+
+
