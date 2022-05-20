6c28aeb38b9b3e4eeb2d8150abe5df8fa5b2e1c6
Yurii Berezan <67425022+YuriiBerezan@users.noreply.github.com>
2022-05-20 10:48:50 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,11 +11,16 @@ const galleryMarkup = createGallerymarkup(galleryItems);
 refs.galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup )
 // console.log(createGallerymarkup(galleryItems))
 
+refs.galleryContainer.addEventListener('click', onGalleryClick)
+
+
+
 function createGallerymarkup(el) {
+  // el.prventDefault();
     return el.map(({preview, original, description}) => {
         return `
           <div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+  <a class="gallery__link" href="${preview}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -28,6 +33,13 @@ function createGallerymarkup(el) {
     }).join(' ');        
 }
 
-
+function onGalleryClick(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  e.prventDefault();
+ console.log(e.target.dataset.sourse)
+  // console.log(e.target)
+}
 
 
